95a4b2e56f9c3d13d104031b81c9a0d23884e04d
Fathimasheril11 <sherilfathima345@gmail.com>
2025-10-09 14:03:46 +0530

    Delete BookingApp directory

--- BookingApp/src/main/java/com/example/BookingApp/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookingApp.repository;
-
-import com.example.BookingApp.entity.BookingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
-}
--- BookingApp/src/main/java/com/example/BookingApp/repository/TheaterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BookingApp.repository;
-
-import com.example.BookingApp.entity.TheaterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TheaterRepository extends JpaRepository<TheaterEntity, Long> {
-}
